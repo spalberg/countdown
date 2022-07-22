ce14f68c4d204b030adea0cb0580bb87647f0f21
Sven Palberg <sven@palberg.de>
2022-07-22 16:25:55 +0000

    zod

--- event/onTarget.ts
+++ event/onTarget.ts
@@ -1,3 +1,33 @@
+import { z } from 'zod';
+
+export const OnTargetRedirectSchema = z.object({
+  type: z.literal('redirect'),
+  url: z.string(),
+});
+
+export const OnTargetYoutubeSchema = z.object({
+  type: z.literal('youtube'),
+  url: z.string(),
+});
+
+export const OnTargetTextSchema = z.object({
+  type: z.literal('text'),
+  text: z.string(),
+  color: z.string().nullish(),
+});
+
+export const OnTargetMarkdownSchema = z.object({
+  type: z.literal('markdown'),
+  markdown: z.string(),
+});
+
+export const OnTargetSchema = z.union([
+  OnTargetRedirectSchema,
+  OnTargetYoutubeSchema,
+  OnTargetTextSchema,
+  OnTargetMarkdownSchema,
+]);
+
 type OnTargetConfigBase<Type extends string, Config extends {}> = {
   type: Type;
 } & Config;
